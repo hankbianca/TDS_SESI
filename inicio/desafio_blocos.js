6fba6754d36f7d2567f1d6e907ecfe4be1c50f20
hankbianca <biancahank2020@gmail.com>
2026-02-26 12:17:31 -0300

    Inicio com JS

---
+++ inicio/desafio_blocos.js
@@ -0,0 +1,15 @@
+/*
+ Desafio: criar dois blocos de código.
+  Onde o primeiro deve imprimir na tela os
+   numeoros pares de 0 a 10 e o segundo
+   bloco deve imprimir na tela os impares
+   */
+
+   {
+    console.log("numeros pares: 0, 2, 4, 6, 8, e 10");
+   }
+
+   {
+    console.log("numeros impares: 1, 3, 5, 7 e 9");
+   }
+   
