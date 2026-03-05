5184a0fbbf9c6f69524acffc79e7f4838e225d46
hankbianca <biancahank2020@gmail.com>
2026-03-05 12:12:31 -0300

    Operadores Aritméticos

---
+++ operadores/aritmeticos.js
@@ -0,0 +1,17 @@
+//valores liteais
+console.log(10 + 5); //adição
+console.log(10 - 5); //subtração
+console.log(10 * 5); //multiplicação
+console.log(10 / 5); //divisão
+console.log(10 % 5); //resto da divisão
+
+// valores variáveis
+
+let a = 100;
+let b = 50;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
