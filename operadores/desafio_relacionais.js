6d9cb2161fe1b9cd72fd9bc907db02a6828ce217
hankbianca <biancahank2020@gmail.com>
2026-03-06 11:39:18 -0300

    Operadores de atribuição e relacionais

---
+++ operadores/desafio_relacionais.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 11;
+
+// Imprimir no console 5 numeros verdadeiros (true)
+console.log(b > a);
+console.log(a < b);
+console.log(b >= a);
+console.log(a <= b);
+console.log(a != b);
+
+// Imprimir no console 5 numeros falsos (false)
+console.log(a > b);
+console.log(b < a);
+console.log(a >= b);
+console.log(b <= a);
+console.log(a == b);
+
---
+++ operadores/relacionais2.js
@@ -0,0 +1,15 @@
+let a = "1"; //Number
+let b = "1"; // String
+let c = "1"; // String
+
+// = Atribuição
+// == Compara somente valor
+// === Compara valor e tipo
+
+console.log(a == b);
+console.log(a === b);
+console.log(b === c);
+
+console.log(a != b);
+console.log(a !== b);
+console.log(b !== c);
