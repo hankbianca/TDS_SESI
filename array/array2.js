56a98d97e340299051d786ed99e2c60997ea8a2f
hankbianca <biancahank2020@gmail.com>
2026-03-13 12:16:38 -0300

    array

---
+++ array/array2.js
@@ -0,0 +1,12 @@
+const nomes = []
+
+//Desafio: inserir 5 novos índices com 5 nomes e imprimir na tela
+
+console.log(nomes[0] = "Bianca");
+console.log(nomes[1] = "Giovana");
+console.log(nomes[2] = "Alana");
+console.log(nomes[3] = "Yohanna");
+console.log(nomes[4] = "Ellen");
+
+console.log(nomes);
+console.log("/n" + nomes.length);
---
+++ array/desafio_array.js
@@ -0,0 +1,26 @@
+const numeros = []
+
+//Inserir 10 numeros utilizando a função .push()
+// e imprimir na tela utilizando o formato: "Número: X"
+
+numeros.push(10);
+numeros.push(20);
+numeros.push(30);
+numeros.push(40);
+numeros.push(50);
+numeros.push(60);
+numeros.push(70);
+numeros.push(80);
+numeros.push(90);
+numeros.push(100);
+
+console.log("Número: " + numeros[0]);
+console.log("Número: " + numeros[1]);
+console.log("Número: " + numeros[2]);
+console.log("Número: " + numeros[3]);
+console.log("Número: " + numeros[4]);
+console.log("Número: " + numeros[5]);
+console.log("Número: " + numeros[6]);
+console.log("Número: " + numeros[7]);
+console.log("Número: " + numeros[8]);
+console.log("Número: " + numeros[9]);
