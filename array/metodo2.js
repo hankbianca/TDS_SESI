88ed03e146cfe843cd223d7c8a7783a3488723a7
hankbianca <biancahank2020@gmail.com>
2026-03-19 12:17:30 -0300

    funções em JS

---
+++ array/metodo2.js
@@ -0,0 +1,9 @@
+const numeros = [1, 2, 3, 4, 5, 8];
+
+//Desafio: remover o último elemento
+
+numeros.pop(); // Remove o último índice
+console.log(numeros);
+
+numeros.splice(1, 2); // Remove o índice informado
+console.log(numeros);
