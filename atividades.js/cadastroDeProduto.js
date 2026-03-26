f3d2b79d11c2c3e3c6f243e1bb79ffc8b885f608
hankbianca <biancahank2020@gmail.com>
2026-03-26 11:54:17 -0300

    atividade 1

---
+++ atividades.js/cadastroDeProduto.js
@@ -0,0 +1,13 @@
+var produto = "Notebook";
+var preco = 2000;
+var preferencia = true;
+
+console.log(`O item é  ${produto} 
+e custa ${preco} reais.`);
+
+if(preferencia = true) {
+console.log("Disponibilidade: true");
+
+} else {
+    console.log("Disponibilidade: false");
+}
