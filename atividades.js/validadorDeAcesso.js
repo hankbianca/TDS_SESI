feecbe85a7a6473d962811f1ad86ceda32c1b666
hankbianca <biancahank2020@gmail.com>
2026-03-27 10:42:48 -0300

    Atividade 3

--- atividades.js/validadorDeAcesso.js
+++ atividades.js/validadorDeAcesso.js
@@ -1,8 +1,12 @@
-var temIdade = 18;
+var temIdade = 15;
 var temConvite = true;
 
 if(temIdade >= 18 && temConvite){
-let resultadoAND = "#AND você pode entrar no evento VIP ";
+let resultadoAND = "você pode entrar no evento VIP ";
 resultadoAND += temIdade && temConvite;
 console.log(resultadoAND);
+
+} else {
+    let resultadoAND = "Você não pode entrar";
+    console.log(resultadoAND);
 }
