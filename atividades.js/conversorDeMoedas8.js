d12189df16e54200c71e7ae7a90e7e50fb22c4af
hankbianca <biancahank2020@gmail.com>
2026-04-02 11:24:46 -0300

    atividade 9

---
+++ atividades.js/conversorDeMoedas8.js
@@ -0,0 +1,9 @@
+// Crie uma função que receba dois parâmetros: o valor em Reais e a taxa do câmbio do dia.
+
+console.log("Digite a entrada e a taxa de cambio, em reais");
+
+function precoFinal(reais, taxaDeCambio){
+    console.log(reais * taxaDeCambio);
+}
+
+precoFinal(20, 5.5)
