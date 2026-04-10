daf76b10a86e84562cb913d0453f906e49e68274
hankbianca <biancahank2020@gmail.com>
2026-04-10 09:39:35 -0300

    Sistema de Notas

---
+++ atividades.js/sistemadeNotas.js
@@ -0,0 +1,13 @@
+let nota1 = 7;
+let nota2 = 7;
+let media = (nota1 + nota2) / 2;
+
+if(media >= 7){
+    console.log("Aprovado!");
+
+} else if(media >= 5 && media <= 6.9){
+console.log("Recuperação!");
+
+} else {
+    console.log("Reprovado")
+}
